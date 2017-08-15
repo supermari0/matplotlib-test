c167bf42b2d0244012e2c58e599d97e3c0bcea1f
Mario Villaplana <mario.villaplana@rackspace.com>
2017-08-15 12:56:47 -0500

    autofmt date ranges and save as png

--- test.py
+++ test.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 
 def main():
     dates = []
-    for i in range(5):
+    for i in range(1000):
         dates.append(datetime.now() - timedelta(days=i))
-    plt.plot(dates, [1,2,3,4,5])
     plt.ylabel('numbers')
-    plt.show()
+    plt.xlabel('dates')
+    fig, ax = plt.subplots()
+    ax.plot_date(dates, range(1000), marker='', linestyle='-')
+    fig.autofmt_xdate()
+    plt.savefig('./test.png')
 
 if __name__ == '__main__':
     main()
